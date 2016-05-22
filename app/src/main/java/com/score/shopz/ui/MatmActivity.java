e112b8be1c37aad6f59f90dae6f59ccfebb1ca33
eranga herath <erangaeb@gmail.com>
2016-05-22 21:44:50 +0800

    payment done

--- app/src/main/java/com/score/shopz/ui/MatmActivity.java
+++ app/src/main/java/com/score/shopz/ui/MatmActivity.java
@@ -173,7 +173,7 @@ public class MatmActivity extends Activity implements NfcAdapter.CreateNdefMessa
             receivedKey = new String(message.getRecords()[0].getPayload());
             Log.d(TAG, "NFC Data received, " + receivedKey);
 
-            // TODO process receive data
+            // process receive data
             onClickPut();
         }
     }
@@ -384,7 +384,25 @@ public class MatmActivity extends Activity implements NfcAdapter.CreateNdefMessa
         if (action.equals("com.score.shopz.DATA_SENZ")) {
             Senz senz = intent.getExtras().getParcelable("SENZ");
 
-            // TODO process response
+            // process response
+            if (senz.getAttributes().containsKey("msg")) {
+                // msg response received
+                ActivityUtils.cancelProgressDialog();
+                isResponseReceived = true;
+                senzCountDownTimer.cancel();
+
+                String msg = senz.getAttributes().get("msg");
+                if (msg != null && msg.equalsIgnoreCase("DONE")) {
+                    Toast.makeText(this, "Payment successful", Toast.LENGTH_LONG).show();
+                } else {
+                    String informationMessage = "Failed to complete the payment";
+                    displayMessageDialog("PUT fail", informationMessage);
+                }
+
+                // exit from activity
+                this.finish();
+                this.overridePendingTransition(R.anim.stay_in, R.anim.bottom_out);
+            }
         }
     }
 
